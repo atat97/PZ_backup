5ce331473507d33048dcad512534f86f8eb13366
atat97 <a_tatusko@wp.pl>
2022-04-14 03:55:42 +0200

    Added the first version of the fuction, but the reference in line 31 need to be fixed.

--- PZ_backup.py
+++ PZ_backup.py
@@ -5,8 +5,31 @@
 #2. Move the recent backup from "new" folder to "old" folder
 #3. Copy the save to the "new" backup folder
 
+import glob
+import os
+import shutil
+
+#Paths TODO: Get user directory automatically; Refactor; Breaks when the folders are empty; 
+latest_save_path = max(glob.glob("C:\\Users\\A\\Zomboid\\Saves\\Sandbox\\*"), key=os.path.getctime) 
+new_backup_save = max(glob.glob("C:\\Users\\A\\Zomboid\\Saves\\Backup\\new\\*"), key=os.path.getctime)
+old_backup_save = max(glob.glob("C:\\Users\\A\\Zomboid\\Saves\\Backup\\old\\*"), key=os.path.getctime)
+
+new_backup_path = "C:\\Users\\A\\Zomboid\\Saves\\Backup\\new"
+old_backup_path = "C:\\Users\\A\\Zomboid\\Saves\\Backup\\old"
+
 def backup_PZ():
-    return 0
+    #1
+    print("Removing old backup...")
+    shutil.rmtree(old_backup_save)
+    print("Done!")
+    #2
+    print("Archiving the current backup...")
+    shutil.move(new_backup_save, old_backup_path)
+    print("Done!")
+    #3
+    print("Backing up the save...")
+    shutil.copytree(latest_save_path, new_backup_path) #TODO: new_backup_path does not exist after it is moved!!!
+    print("Done!")
 
 if __name__ == "__main__":
-    print("")
+    backup_PZ()

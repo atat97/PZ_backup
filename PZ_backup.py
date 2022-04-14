e0d7908781fb701749c9a840e044b4f42e7b4a24
atat97 <a_tatusko@wp.pl>
2022-04-14 03:26:47 +0200

    first commit

---
+++ PZ_backup.py
@@ -0,0 +1,12 @@
+#Project Zomboid Backup Saves
+
+#TODO:
+#1. Remove backup from the "old" folder
+#2. Move the recent backup from "new" folder to "old" folder
+#3. Copy the save to the "new" backup folder
+
+def backup_PZ():
+    return 0
+
+if __name__ == "__main__":
+    print("")
